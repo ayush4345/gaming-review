36ef89cc91f3569c682bef3a501716c1a0ed893a
ayush4345 <sanusingh335@gmail.com>
2023-07-07 13:10:54 +0530

    create svelte app

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<div class="index">
+	<h1>Welcome to Sveltekit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+    .index{
+        text-align: center;
+    }
+</style>
